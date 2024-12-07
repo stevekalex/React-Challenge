2756753dcec3f7dfe9d411aa5c8edddaddbfa9eb
Steve A <stevekalex@gmail.com>
2024-12-07 13:57:38 +0000

    feat: enable pause and stop button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import { FizzBuzz } from './fizzBuzz'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+export enum State {
+  Running = "running",
+  Paused = "paused",
+  Stopped = "stopped"
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [number, setNumber] = useState(0)
+  const [state, setState] = useState(State.Paused)
+  const [error, setError] = useState("")
+
+  const handleNumberChanged = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target
+
+    const number = Number(value)
+
+    if (!isNaN(number)) {
+      setNumber(number)
+      setError("")
+    } else {
+      setError("Please input a number")
+    }
+  }
+
+  const startOrPauseProgramme = () => {
+    if (state == State.Paused || state === State.Stopped) setState(State.Running)
+    if (state == State.Running) setState(State.Paused)
+  }
+
+  const disableProgramme = () => {
+    setState(State.Stopped)
+    setNumber(0)
+  }
+
+  const renderButtonText = () => {
+    if (state === State.Running) return "Pause"
+    return "Start"
+  }
 
   return (
     <>
@@ -17,10 +53,14 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          {FizzBuzz({ number: count })}
-        </button>
+      <div className="card"> 
+        <input value={number} onInput={handleNumberChanged} /> 
+        <div className="button-container">
+          <button onClick={() => startOrPauseProgramme()}>{renderButtonText()}</button>
+          <button onClick={() => disableProgramme()}>STOP</button>
+        </div>
+        <FizzBuzz number={number} state={state}/>
+        {error}
       </div>
     </>
   )
--- src/fizzBuzz.tsx
+++ src/fizzBuzz.tsx
@@ -1,10 +1,28 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import { State } from './App';
 
 interface FizzBuzzProps {
   number: number;
+  state: State;
 }
 
-export const FizzBuzz: FC<FizzBuzzProps> = ({ number }) => {
+export const FizzBuzz: FC<FizzBuzzProps> = ({ number, state }) => {
+  const [currentNumber, setCurrentNumber] = useState(0)
+
+  useEffect(() => {
+    let intervalId: any;
+    if (state === State.Running && currentNumber < number) {
+      intervalId = setInterval(() => setCurrentNumber(currentNumber + 1), 250)
+    } 
+
+    if (state === State.Stopped) {
+      intervalId = setInterval(() => setCurrentNumber(0), 0)
+    }
+
+
+    return () => clearInterval(intervalId)
+  }, [currentNumber, state])
+
   const calculateFizzBuzz = (n: number): string => {
     const isDivisibleByThree = n % 3 === 0;
     const isDivisibleByFive = n % 5 === 0;
@@ -18,7 +36,7 @@ export const FizzBuzz: FC<FizzBuzzProps> = ({ number }) => {
 
   return (
     <div>
-      {calculateFizzBuzz(number)}
+      {calculateFizzBuzz(currentNumber)}
     </div>
   );
 };
